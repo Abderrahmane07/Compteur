b1e7ce93d549e7b7568a36ba92063f72b5b50882
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2022-11-19 01:06:07 +0100

    Timer working in the main screen and test page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,107 +12,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final tempsInitial = DateTime.now();
-  Timer? timer;
-  String temps = "coco";
-  Stopwatch stopwatch = Stopwatch();
+  int timeFirstClient = 5;
 
-  void leftButtonPressed() {
-    setState(() {
-      if (stopwatch.isRunning) {
-        print("${stopwatch.elapsedMilliseconds}");
-      } else {
-        stopwatch.reset();
-      }
-    });
-  }
-
-  void rightButtonPressed() {
-    setState(() {
-      if (stopwatch.isRunning) {
-        stopwatch.stop();
-      } else {
-        stopwatch.start();
-      }
-    });
-  }
-
-  void prixSelontemps() {
-    setState(() {
-      var tempsTempo = DateTime.now();
-      int difference = tempsTempo.difference(tempsInitial).inSeconds;
-      // updateState();
-      timer = Timer.periodic(Duration(seconds: 1), (timer) {
-        difference++;
-        temps = difference.toString();
+  void _startCountDown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        timeFirstClient++;
       });
     });
   }
-  // int seconds = 0, minutes = 0, hours = 0;
-  // String digitSeconds = "00", digitMinutes = "00", digitHours = "00";
+  // final tempsInitial = DateTime.now();
   // Timer? timer;
-  // bool started = false;
-  // List laps = [];
+  // String temps = "coco";
+  // Stopwatch stopwatch = Stopwatch();
 
-  // void stop() {
-  //   timer!.cancel();
+  // void leftButtonPressed() {
   //   setState(() {
-  //     started = false;
-  //   });
-  // }
-
-  // void reset() {
-  //   timer!.cancel();
-  //   setState(() {
-  //     seconds = 0;
-  //     minutes = 0;
-  //     hours = 0;
-
-  //     digitSeconds = "00";
-  //     digitMinutes = "00";
-  //     digitHours = "00";
-
-  //     started = false;
-  //   });
-  // }
-
-  // void addLaps() {
-  //   String lap = "$digitHours:$digitMinutes:$digitSeconds";
-  //   setState(() {
-  //     laps.add(lap);
-  //   });
-  // }
-
-  // void start() {
-  //   started = true;
-  //   timer = Timer.periodic(Duration(seconds: 1), (timer) {
-  //     int localSeconds = seconds + 1;
-  //     int localMinutes = minutes;
-  //     int localHours = hours;
-
-  //     if (localSeconds > 59) {
-  //       if (localMinutes > 59) {
-  //         localHours++;
-  //         localMinutes = 0;
-  //       } else {
-  //         localMinutes++;
-  //         localSeconds = 0;
-  //       }
+  //     if (stopwatch.isRunning) {
+  //       print("${stopwatch.elapsedMilliseconds}");
+  //     } else {
+  //       stopwatch.reset();
   //     }
-  //     setState(() {
-  //       seconds = localSeconds;
-  //       minutes = localMinutes;
-  //       hours = localHours;
-  //       digitSeconds = (seconds >= 10) ? "$seconds" : "0$seconds";
-  //       digitSeconds = (minutes >= 10) ? "$minutes" : "0$minutes";
-  //       digitSeconds = (hours >= 10) ? "$hours" : "0$hours";
+  //   });
+  // }
+
+  // void rightButtonPressed() {
+  //   setState(() {
+  //     if (stopwatch.isRunning) {
+  //       stopwatch.stop();
+  //     } else {
+  //       stopwatch.start();
+  //     }
+  //   });
+  // }
+
+  // void prixSelontemps() {
+  //   setState(() {
+  //     var tempsTempo = DateTime.now();
+  //     int difference = tempsTempo.difference(tempsInitial).inSeconds;
+  //     // updateState();
+  //     timer = Timer.periodic(Duration(seconds: 1), (timer) {
+  //       difference++;
+  //       temps = difference.toString();
   //     });
   //   });
   // }
 
   @override
   Widget build(BuildContext context) {
-    prixSelontemps();
+    // prixSelontemps();
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 67, 61, 61),
       body: Row(
@@ -138,7 +86,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         backgroundColor: const Color.fromARGB(255, 48, 182, 97),
                       ),
                       onPressed: () {
-                        rightButtonPressed();
+                        _startCountDown();
+                        // rightButtonPressed();
                       },
                       child: const Text(''),
                     ),
@@ -159,7 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             const Color.fromARGB(255, 208, 183, 153),
                       ),
                       onPressed: () {
-                        leftButtonPressed();
+                        // leftButtonPressed();
                       },
                       child: const Text(''),
                     ),
@@ -226,9 +175,11 @@ class _HomeScreenState extends State<HomeScreen> {
             decoration: const BoxDecoration(
               color: Color.fromARGB(255, 0, 0, 0),
             ),
+            // ignore: prefer_const_constructors
             child: Center(
+              // ignore: prefer_const_constructors
               child: SevenSegmentDisplay(
-                value: temps,
+                value: timeFirstClient.toString(),
                 size: 12.0,
               ),
               // SevenSegmentDisplay(
@@ -236,9 +187,6 @@ class _HomeScreenState extends State<HomeScreen> {
               //   size: 12.0,
               // ),
             ),
-            // DecoratedBox(
-            //   decoration: BoxDecoration(color: Colors.red),
-            // ),
           ),
           Expanded(child: Container()),
           // bloc 3: ad
--- lib/tests_screen.dart
+++ lib/tests_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class TestsScreen extends StatefulWidget {
@@ -8,8 +10,38 @@ class TestsScreen extends StatefulWidget {
 }
 
 class _TestsScreenState extends State<TestsScreen> {
+  int timeLeft = 5;
+
+  void _startCountDown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        timeLeft++;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              timeLeft.toString(),
+              style: const TextStyle(fontSize: 40),
+            ),
+            MaterialButton(
+                onPressed: _startCountDown,
+                // ignore: sort_child_properties_last
+                child: const Text(
+                  'Start',
+                  style: TextStyle(fontSize: 20),
+                ),
+                color: Colors.white70),
+          ],
+        ),
+      ),
+    );
   }
 }
